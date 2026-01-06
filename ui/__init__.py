8d2ea9bda858e48b7b8f53781f78c3d506f404b6
Gilbert Young Jr. <gilbertjyoungjr@gmail.com>
2026-01-06 07:04:59 -0600

    add logic to enable streaming ui

---
+++ ui/__init__.py
@@ -0,0 +1,28 @@
+from .terminal import (
+    console,
+    print_header,
+    print_stats_preview,
+    print_generating_spinner,
+    print_success,
+    print_error,
+    print_chat_header,
+    print_agent_response,
+    print_user_prompt,
+    print_suggestions,
+    StreamingCallback,
+)
+
+__all__ = [
+    "console",
+    "print_header",
+    "print_stats_preview", 
+    "print_generating_spinner",
+    "print_success",
+    "print_error",
+    "print_chat_header",
+    "print_agent_response",
+    "print_user_prompt",
+    "print_suggestions",
+    "StreamingCallback",
+]
+
